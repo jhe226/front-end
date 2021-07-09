37e2a964ae568dcdaf644e3006ffa5c2d48cc035
jhe226 <79580469+jhe226@users.noreply.github.com>
2021-07-09 11:17:32 +0900

    Add files via upload

---
+++ Part.2/js/text-color.js
@@ -0,0 +1,6 @@
+ // heading 변수를 선언하고 h1 태그를 저장한 것.
+ var heading = document.querySelector("#heading");
+ // h1 태그를 클릭하면 글자 색깔을 red로 설정하시오.
+ heading.onclick = function(){
+     heading.style.color = "red";
+ }
