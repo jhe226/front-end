505539f4c59373392597b0946a4d5145c52ee3f0
jhe226 <79580469+jhe226@users.noreply.github.com>
2021-07-14 23:43:10 +0900

    Add files via upload

---
+++ Part.5 함수와 이벤트/js/earth.js
@@ -0,0 +1,37 @@
+// 전역변수 선언
+var $earth = null;
+$(document).ready(function(){
+    initialize();
+    event_earth();
+
+});
+// 전역변수 초기화
+function initialize()
+{
+    // 지구본 찾기
+    $earth = $("#earth");
+}
+// 이벤트와 관련된 내용을 함수로 등록
+function event_earth() {
+    // 버튼에 이벤트 등록
+    $("#btnStart").click(function(){
+    // 지구본 움직이기
+    // 지구본 위치 값 구하기
+    var x = parseInt($("#txtX").val());
+    var y = parseInt($("#txtY").val());
+    moveEarth(x, y);
+    });
+}
+// 지구본의 움직임을 구현한 함수
+function moveEarth(x, y) {
+    if((x >= 0 && x <= 500) && (y >= 0 && y <= 300))
+    {
+        $earth.css({
+        left : x,
+        top : y
+        });
+    }
+    else{
+        alert("입력된 값이 범위를 벗어났습니다.");
+    }
+}
